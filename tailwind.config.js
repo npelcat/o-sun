26e0c2e4c7f3616a53a381b543d7db4222cd44c2
npelcat <pelcat.nd@gmail.com>
2024-06-19 10:04:36 +0200

    new router app

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,10 @@ const { title } = require("process");
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,ts,tsx,jsx}"],
+  content: [
+    "./app/**/*.{js,ts,tsx,jsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {
       colors: {
