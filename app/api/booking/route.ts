492a9487b4eb13d6c92f4b6b0fb9efafe0c98d0b
npelcat <pelcat.nd@gmail.com>
2025-03-07 17:37:31 +0100

    :sparkles: add functionnal user interface for booking + checking correct time slot exists before sending the form data

--- app/api/booking/route.ts
+++ app/api/booking/route.ts
@@ -1,24 +1,50 @@
 import { NextResponse, NextRequest } from "next/server";
 import db from "@/src/db/index";
 import { bookings, formData } from "@/src/db/schema";
+import { withErrorHandler, HttpError } from "@/utils/withErrorHandler";
+import { timeSlots } from "@/src/db/schema";
+import { eq } from "drizzle-orm";
+import logger from "@/utils/logger";
 
-export async function GET() {
-  try {
+export async function GET(req: NextRequest) {
+  return withErrorHandler(req, async () => {
+    logger.info("GET /booking - Retrieving all reservations");
     const reservations = await db.select().from(bookings);
+    logger.info(`GET /booking - Retrieved ${reservations.length} reservations`);
     return NextResponse.json({ reservations });
-  } catch (error) {
-    console.error(error);
-    return NextResponse.json(
-      { error: "Erreur lors de la récupération des réservations" },
-      { status: 500 }
-    );
-  }
+  });
 }
 
 export async function POST(req: NextRequest) {
-  try {
+  return withErrorHandler(req, async () => {
     const body = await req.json();
     const { timeSlotId, name, email, content } = body;
+
+    logger.info("POST /booking - Attempting to create a reservation", {
+      timeSlotId,
+      name,
+      email,
+    });
+
+    if (!timeSlotId || !name || !email) {
+      logger.error("POST /booking - Missing or invalid data");
+      throw new HttpError(400, "Données manquantes ou invalides");
+    }
+
+    const timeSlot = await db
+      .select()
+      .from(timeSlots)
+      .where(eq(timeSlots.id, timeSlotId))
+      .limit(1)
+      .execute();
+
+    if (!timeSlot) {
+      logger.error(`POST /booking - Time slot ${timeSlotId} unavailable`);
+      throw new HttpError(
+        404,
+        "Ce créneau horaire est malheureusement indisponible"
+      );
+    }
 
     const insertedFormData = await db
       .insert(formData)
@@ -30,6 +56,7 @@ export async function POST(req: NextRequest) {
       .returning();
 
     const formId = insertedFormData[0].id;
+    logger.info("POST /booking - Form data inserted", { formId });
 
     const insertedBooking = await db
       .insert(bookings)
@@ -40,12 +67,9 @@ export async function POST(req: NextRequest) {
       })
       .returning();
 
+    logger.info("POST /booking - Reservation created", {
+      bookingId: insertedBooking[0].id,
+    });
     return NextResponse.json({ booking: insertedBooking[0] }, { status: 201 });
-  } catch (error) {
-    console.error(error);
-    return NextResponse.json(
-      { error: "Erreur lors de la création de la réservation" },
-      { status: 500 }
-    );
-  }
+  });
 }
