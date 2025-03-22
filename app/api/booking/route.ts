cbe30f0726b0cfe5b67b3c9433fac0bbd1755c91
npelcat <pelcat.nd@gmail.com>
2025-03-22 13:03:58 +0100

    :loud_sound: add logs in routes

--- app/api/booking/route.ts
+++ app/api/booking/route.ts
@@ -4,10 +4,13 @@ import { bookings, formData } from "@/src/db/schema";
 import { withErrorHandler, HttpError } from "@/utils/withErrorHandler";
 import { timeSlots } from "@/src/db/schema";
 import { eq } from "drizzle-orm";
+import logger from "@/utils/logger";
 
 export async function GET(req: NextRequest) {
   return withErrorHandler(req, async () => {
+    logger.info("GET /booking - Retrieving all reservations");
     const reservations = await db.select().from(bookings);
+    logger.info(`GET /booking - Retrieved ${reservations.length} reservations`);
     return NextResponse.json({ reservations });
   });
 }
@@ -17,7 +20,14 @@ export async function POST(req: NextRequest) {
     const body = await req.json();
     const { timeSlotId, name, email, content } = body;
 
+    logger.info("POST /booking - Attempting to create a reservation", {
+      timeSlotId,
+      name,
+      email,
+    });
+
     if (!timeSlotId || !name || !email) {
+      logger.error("POST /booking - Missing or invalid data");
       throw new HttpError(400, "Données manquantes ou invalides");
     }
 
@@ -29,6 +39,7 @@ export async function POST(req: NextRequest) {
       .execute();
 
     if (!timeSlot) {
+      logger.error(`POST /booking - Time slot ${timeSlotId} unavailable`);
       throw new HttpError(
         404,
         "Ce créneau horaire est malheureusement indisponible"
@@ -45,6 +56,7 @@ export async function POST(req: NextRequest) {
       .returning();
 
     const formId = insertedFormData[0].id;
+    logger.info("POST /booking - Form data inserted", { formId });
 
     const insertedBooking = await db
       .insert(bookings)
@@ -55,6 +67,9 @@ export async function POST(req: NextRequest) {
       })
       .returning();
 
+    logger.info("POST /booking - Reservation created", {
+      bookingId: insertedBooking[0].id,
+    });
     return NextResponse.json({ booking: insertedBooking[0] }, { status: 201 });
   });
 }
