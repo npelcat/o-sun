c4b29c7eb989d75da49ccc3b68b077b012a84722
npelcat <pelcat.nd@gmail.com>
2025-03-22 13:03:58 +0100

    :loud_sound: add logs in routes

--- app/api/booking/[id]/route.ts
+++ app/api/booking/[id]/route.ts
@@ -3,6 +3,7 @@ import db from "@/src/db/index";
 import { bookings } from "@/src/db/schema";
 import { eq } from "drizzle-orm";
 import { withErrorHandler, HttpError } from "@/utils/withErrorHandler";
+import logger from "@/utils/logger";
 
 export async function GET(
   req: NextRequest,
@@ -10,13 +11,16 @@ export async function GET(
 ) {
   return withErrorHandler(req, async () => {
     const { id } = params;
+    logger.info(`GET /booking/${id} - Retrieving reservation`);
     const reservation = await db
       .select()
       .from(bookings)
       .where(eq(bookings.id, id));
     if (!reservation.length) {
+      logger.error(`Reservation not found for id: ${id}`);
       throw new HttpError(404, "Réservation introuvable");
     }
+    logger.info(`Reservation found for id: ${id}`);
     return NextResponse.json({ reservation: reservation[0] });
   });
 }
@@ -30,7 +34,13 @@ export async function PUT(
     const body = await req.json();
     const { timeSlotId, status } = body;
 
+    logger.info(`PUT /booking/${id} - Updating reservation`, {
+      timeSlotId,
+      status,
+    });
+
     if (!timeSlotId || !status) {
+      logger.error(`PUT /booking/${id} - Missing or invalid data`);
       throw new HttpError(400, "Données manquantes ou invalides");
     }
 
@@ -41,8 +51,10 @@ export async function PUT(
       .returning();
 
     if (!updatedReservation.length) {
+      logger.error(`PUT /booking/${id} - Reservation not found or not updated`);
       throw new HttpError(404, "Réservation introuvable ou non mise à jour");
     }
+    logger.info(`PUT /booking/${id} - Reservation successfully updated`);
     return NextResponse.json({ reservation: updatedReservation[0] });
   });
 }
@@ -53,14 +65,19 @@ export async function DELETE(
 ) {
   return withErrorHandler(req, async () => {
     const { id } = params;
+    logger.info(`DELETE /booking/${id} - Deleting reservation`);
     const deletedReservation = await db
       .delete(bookings)
       .where(eq(bookings.id, id))
       .returning();
 
     if (!deletedReservation.length) {
+      logger.error(
+        `DELETE /booking/${id} - Reservation not found or not deleted`
+      );
       throw new HttpError(404, "Réservation introuvable ou non supprimée");
     }
+    logger.info(`DELETE /booking/${id} - Reservation successfully deleted`);
     return NextResponse.json({ message: "Réservation supprimée" });
   });
 }
