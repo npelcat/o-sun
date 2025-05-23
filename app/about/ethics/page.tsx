a0d478a8c2c8a6986cfdfb09f3863d5ab8cd2033
npelcat <pelcat.nd@gmail.com>
2025-05-23 11:31:19 +0200

    :lipstick: improve design + add .env.example

--- app/about/ethics/page.tsx
+++ app/about/ethics/page.tsx
@@ -32,69 +32,62 @@ const Ethics: React.FC = () => {
     (block) => block.slug === "ma-facon-de-travailler"
   );
 
-  const ethicsContents = accordions.filter(
-    (block) =>
-      block.slug &&
-      [
-        "mon-ethique-en-communication-animale",
-        "mon-ethique-en-soins-energetiques",
-        "mon-ethique-pour-les-services-aux-gardiens",
-      ].includes(block.slug)
+  const ethicsContents = accordions.filter((block) =>
+    accordionSlugs.includes(block.slug || "")
   );
 
   return (
     <div className="pt-16 space-y-12">
-      <h2 className="text-center text-3xl pt-16 pb-5 px-4 font-subtitle font-bold">
+      <h2 className="text-center text-3xl font-subtitle font-bold px-4">
         Mon éthique
       </h2>
 
       <div className="flex justify-center bg-beige">
-        <div className="py-8 w-full md:w-3/5  px-4">
-          <div>
-            {wayOfWorking && (
-              <>
-                <CardTitlePhoto
-                  title={wayOfWorking.title}
-                  image={wayOfWorking.picture?.url || ""}
-                  alt={wayOfWorking.picture?.alternativeText || ""}
-                />
-                <section className="pt-8 text-justify">
-                  <BlockRendererClient content={wayOfWorking.content} />
-                  <div className="flex justify-center py-8">
-                    <Image
-                      className="w-1/2 h-full md:w-1/6 item-center object-cover"
-                      loading="lazy"
-                      src="https://res.cloudinary.com/dqpkzbkca/image/upload/v1720859313/dog-1532627_1920_soflbg.png"
-                      alt=""
-                      aria-hidden="true"
-                      width="1920"
-                      height="1180"
-                    />
-                  </div>
-                </section>
-              </>
-            )}
-            <div className="bg-dark-beige rounded-lg my-8 p-2">
-              <h3 className="text-center font-bold mt-8 bg-white bg-opacity-50 rounded-lg p-2">
-                Les détails de ma pratique pour chaque discipline :
-              </h3>
-              <div className="p-2 text-justify">
-                {ethicsContents.map((content) => (
-                  <Accordion key={content.slug} title={content.title}>
-                    <BlockRendererClient content={content.content} />
-                  </Accordion>
-                ))}
-              </div>
+        <div className="w-full md:w-3/5 py-8 px-4">
+          {wayOfWorking && (
+            <>
+              <CardTitlePhoto
+                title={wayOfWorking.title}
+                image={wayOfWorking.picture?.url || ""}
+                alt={wayOfWorking.picture?.alternativeText || ""}
+              />
+              <section className="pt-8 text-justify">
+                <BlockRendererClient content={wayOfWorking.content} />
+                <div className="flex justify-center py-8">
+                  <Image
+                    className="w-1/2 md:w-1/6 object-cover"
+                    loading="lazy"
+                    src="https://res.cloudinary.com/dqpkzbkca/image/upload/v1720859313/dog-1532627_1920_soflbg.png"
+                    alt=""
+                    aria-hidden="true"
+                    width={1920}
+                    height={1180}
+                  />
+                </div>
+              </section>
+            </>
+          )}
+
+          <div className="bg-dark-beige rounded-lg my-8 p-4">
+            <h3 className="text-center font-bold bg-white bg-opacity-50 rounded-lg p-2">
+              Les détails de ma pratique pour chaque discipline :
+            </h3>
+            <div className="text-justify">
+              {ethicsContents.map((content) => (
+                <Accordion key={content.slug} title={content.title}>
+                  <BlockRendererClient content={content.content} />
+                </Accordion>
+              ))}
             </div>
           </div>
         </div>
       </div>
 
-      <div className="flex justify-center mt-16">
+      <div className="flex justify-center pb-12">
         <Button
           titleButton="Me contacter"
           lien="/contact"
-          className="flex justify-center items-center mt-8 text-xl text-white bg-dark-green font-subtitle rounded-full p-4 text-center transition duration-300 ease-in-out hover:bg-dark-beige hover:text-black focus:outline-none focus:ring-2 focus:ring-dark-green focus:ring-offset-2"
+          className="text-xl text-white bg-dark-green font-subtitle rounded-full px-6 py-3 transition hover:bg-dark-beige hover:text-black focus:outline-none focus:ring-2 focus:ring-dark-green focus:ring-offset-2"
         />
       </div>
     </div>
--- app/about/page.tsx
+++ app/about/page.tsx
@@ -24,68 +24,60 @@ const AboutIndex: NextPage = () => {
     accordionSlugs
   );
 
-  if (error)
+  if (error) {
     return (
       <ErrorDisplay message={error} onRetry={() => window.location.reload()} />
     );
+  }
+
   if (!blockContents || !accordions) return <Loader />;
 
-  const oceaneContent = blockContents.find((block) => block.slug === "oceane");
+  const oceaneContent = blockContents.find((b) => b.slug === "oceane");
   const diplomesEtFormations = blockContents.find(
-    (block) => block.slug === "mes-diplomes-et-formations"
+    (b) => b.slug === "mes-diplomes-et-formations"
   );
-  const trainingContents = accordions.filter(
-    (block) =>
-      block.slug &&
-      [
-        "ma-formation-en-communication-animale",
-        "ma-formation-en-soins-energetiques",
-        "ma-formation-pour-les-services-aux-gardiens",
-        "mon-ancienne-vie",
-      ].includes(block.slug)
+
+  const trainingContents = accordions.filter((b) =>
+    accordionSlugs.includes(b.slug || "")
   );
 
   return (
-    <div className="text-center pt-16 space-y-12">
-      <h2 className="text-3xl pt-16 pb-5 px-4 font-subtitle font-bold">
+    <div className="pt-16 space-y-12 text-center">
+      <h2 className="text-3xl font-bold font-subtitle px-4 pt-16 pb-5">
         Qui suis-je ?
       </h2>
 
       {oceaneContent && (
-        <div className="flex justify-center bg-beige">
-          <div className="py-8 w-full md:w-3/5 px-4">
+        <section className="flex justify-center bg-beige">
+          <div className="w-full md:w-3/5 py-8 px-4 space-y-8 text-justify">
             <CardTitlePhoto
               title={oceaneContent.title}
               image={oceaneContent.picture?.url || ""}
               alt={oceaneContent.picture?.alternativeText || ""}
             />
-            <div className="text-justify">
-              <BlockRendererClient content={oceaneContent.content} />
-            </div>
+            <BlockRendererClient content={oceaneContent.content} />
             <div className="flex justify-center py-8">
               <Image
-                className="w-1/2 h-full md:w-1/6 item-center object-cover"
-                loading="lazy"
                 src="https://res.cloudinary.com/dqpkzbkca/image/upload/v1720856658/fox-7405603_1920_etg9z1.png"
                 alt=""
                 aria-hidden="true"
                 width={1920}
                 height={1358}
+                loading="lazy"
+                className="object-cover w-1/2 md:w-1/6"
               />
             </div>
-            <p>
-              <strong>
-                Pour en savoir plus, vous pouvez consulter la partie diplômes et
-                formations ci-dessous.
-              </strong>
+            <p className="font-semibold text-center">
+              Pour en savoir plus, vous pouvez consulter la partie diplômes et
+              formations ci-dessous.
             </p>
           </div>
-        </div>
+        </section>
       )}
 
       {diplomesEtFormations && (
-        <div className="flex justify-center bg-dark-beige">
-          <div className="py-8 w-full md:w-3/5 px-4">
+        <section className="flex justify-center bg-dark-beige">
+          <div className="w-full md:w-3/5 py-8 px-4 space-y-8 text-justify">
             <CardTitlePhoto
               title={diplomesEtFormations.title}
               image={diplomesEtFormations.picture?.url || ""}
@@ -99,14 +91,14 @@ const AboutIndex: NextPage = () => {
               </Accordion>
             ))}
           </div>
-        </div>
+        </section>
       )}
 
       <div className="flex justify-center mt-16">
         <Button
           titleButton="Réserver un service"
           lien="/contact/booking"
-          className="flex justify-center items-center mt-8 text-xl text-white bg-dark-green font-subtitle rounded-full p-4 text-center transition duration-300 ease-in-out hover:bg-dark-beige hover:text-black focus:outline-none focus:ring-2 focus:ring-dark-green focus:ring-offset-2"
+          className="mt-8 p-4 text-xl font-subtitle text-white bg-dark-green rounded-full transition duration-300 hover:bg-dark-beige hover:text-black focus:outline-none focus:ring-2 focus:ring-dark-green focus:ring-offset-2"
         />
       </div>
     </div>
