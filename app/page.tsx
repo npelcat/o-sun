77d2a67198b8c9ca85d2a82804e1f95d966429a5
npelcat <pelcat.nd@gmail.com>
2025-07-10 11:37:25 +0200

    :children_crossing: put username in admin page

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import { pageMetadata } from "@/app/lib/metadata";
+import { pageMetadata } from "@/lib/metadata";
 import {
   fetchLinkComponentById,
   fetchMultipleLinkComponents,
--- app/services/page.tsx
+++ app/services/page.tsx
@@ -1,4 +1,4 @@
-import { pageMetadata } from "@/app/lib/metadata";
+import { pageMetadata } from "@/lib/metadata";
 import { fetchBlockContentById } from "@/app/api/strapi/fetchers/block-content";
 import { fetchMultipleAccordions } from "@/app/api/strapi/fetchers/accordion";
 import AnimalCommunicationClient from "@/src/pageComponents/AnimalCommunicationClient";
